35d654caef7b9b0fd76e7b488c3ac7e40f1afe4d
stefanomontr <montrasio97@gmail.com>
2024-02-14 14:49:47 +0100

    init commit

---
+++ src/main/java/com/fabrick/api/fabrickaccountapi/FabrickAccountApiApplication.java
@@ -0,0 +1,13 @@
+package com.fabrick.api.fabrickaccountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickAccountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickAccountApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrick/api/fabrickaccountapi/FabrickAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrick.api.fabrickaccountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
